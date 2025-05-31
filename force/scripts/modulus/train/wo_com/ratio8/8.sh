913327858cbb9800b3f1897ea0fbc34f255e586a
zhuochenn <earlychenz@hotmail.com>
2025-05-31 12:42:57 +0100

    latest

--- force/scripts/modulus/train/wo_com/ratio8/8.sh
+++ force/scripts/modulus/train/wo_com/ratio8/8.sh
@@ -1,10 +1,10 @@
 
 
 python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/6_8.yaml
-python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/10_8.yaml
-python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/12_8.yaml
-python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/14_8.yaml
-python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/16_8.yaml
-python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/18_8.yaml
+# python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/10_8.yaml
+# python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/12_8.yaml
+# python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/14_8.yaml
+# python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/16_8.yaml
+# python force/wo_com/train_force.py --config force/scripts/modulus/train/wo_com/ratio8/18_8.yaml
 
 
